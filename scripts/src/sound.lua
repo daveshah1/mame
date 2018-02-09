f8872b10f54cc5548d647f9922e44a3c554aa5ad
David Shah <davey1576@gmail.com>
2018-02-09 20:12:21 +0000

    Start work on a VTxx APU implementation

--- scripts/src/sound.lua
+++ scripts/src/sound.lua
@@ -628,6 +628,8 @@ end
 ---------------------------------------------------
 -- Nintendo custom sound chips
 --@src/devices/sound/nes_apu.h,SOUNDS["NES_APU"] = true
+--@src/devices/sound/nes_vt_apu.h,SOUNDS["NES_APU"] = true
+
 ---------------------------------------------------
 
 if (SOUNDS["NES_APU"]~=null) then
@@ -635,6 +637,8 @@ if (SOUNDS["NES_APU"]~=null) then
 		MAME_DIR .. "src/devices/sound/nes_apu.cpp",
 		MAME_DIR .. "src/devices/sound/nes_apu.h",
 		MAME_DIR .. "src/devices/sound/nes_defs.h",
+		MAME_DIR .. "src/devices/sound/nes_vt_apu.cpp",
+		MAME_DIR .. "src/devices/sound/nes_vt_apu.h"
 	}
 end
 
